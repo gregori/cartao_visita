fab0257e22169167c3c96fc7dc274991c2f43b20
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2022-08-17 22:11:24 -0300

    Final da aula 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(),
-    ),
-  ));
+  runApp(const BusinessCard());
+}
+
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Container(
+            child: Text("Hello World"),
+            color: Colors.white,
+            width: 100.0,
+            height: 100.0,
+            // margin: EdgeInsets.all(30.0),  // todas as margens
+            // margin: EdgeInsets.symmetric(
+            //   vertical: 10.0,
+            //   horizontal: 50.0,
+            // ),  // simétrico
+            // margin: EdgeInsets.fromLTRB(30, 10, 20, 50), // left right top bottom
+            margin: EdgeInsets.only(left: 40.0),
+          ),
+        ),
+      ),
+    );
+  }
 }
