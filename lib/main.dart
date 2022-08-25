1cf0e6979a4e69a4cdbe404db4e8a9c8ccae3fa2
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2022-08-24 21:11:39 -0300

    Exercício leiaute

--- lib/main.dart
+++ lib/main.dart
@@ -14,39 +14,31 @@ class BusinessCard extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Column(
-            // verticalDirection: VerticalDirection.up, // de baixo para cima
-            // verticalDirection: VerticalDirection.down, // de cima para baixo
-            // mainAxisAlignment: MainAxisAlignment.start,  // padrão - do início da tela
-            // mainAxisAlignment: MainAxisAlignment.end,  // do final da tela
-            // mainAxisAlignment: MainAxisAlignment.center,  // no centro da tela
-            // mainAxisAlignment: MainAxisAlignment.spaceEvenly,  // igualmente espaçado
-            // mainAxisAlignment: MainAxisAlignment.spaceBetween,  // espalhado na tela
-            crossAxisAlignment: CrossAxisAlignment.end,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,  // espalhado na tela
             children: [
-              Container(
-                child: Text("Hello World"),
-                color: Colors.white,
-                width: 100.0,
-                height: 100.0,
-                // margin: EdgeInsets.all(30.0),  // todas as margens
-                // margin: EdgeInsets.symmetric(
-                //   vertical: 10.0,
-                //   horizontal: 50.0,
-                // ),  // simétrico
-                // margin: EdgeInsets.fromLTRB(30, 10, 20, 50), // left right top bottom
-              ),
-              SizedBox(height: 10,),
-              Container(
-                color: Colors.blue,
-                width: 100.0,
-                height: 100.0,
-              ),
-              SizedBox(height: 10,),
               Container(
                 color: Colors.red,
                 width: 100.0,
-                height: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    color: Colors.yellow,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                  Container(
+                    color: Colors.lightGreen,
+                    width: 100.0,
+                    height: 100.0,
+                  ),
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
               ),
             ],
           ),
